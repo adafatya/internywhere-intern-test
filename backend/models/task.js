7655fe77453b2fc5dcf78affb462f462b4ec44c0
adafatya <aufaditya1895@gmail.com>
2023-12-10 13:11:14 +0700

    add rerad detail, update, and delete

--- backend/models/task.js
+++ backend/models/task.js
@@ -21,12 +21,6 @@ exports.getTask = (payload) => {
     WHERE is_deleted = false
     ORDER BY ? ?
     LIMIT ? OFFSET ?`
-    const payloadValues = [
-        payload.sortColumn,
-        payload.sortOrder,
-        payload.limit,
-        payload.offset
-    ]
 
     return new Promise((resolve, reject) => {
         db.query(query, payload, function (error, results, fields) {
@@ -34,6 +28,57 @@ exports.getTask = (payload) => {
                 reject(error)
             } else {
                 resolve(results)
+            }
+        })
+    })
+}
+
+exports.getTaskDetail = (payload) => {
+    const query = `
+    SELECT task_name, task_description, task_due_date
+    FROM tasks
+    WHERE task_id = ?`
+
+    return new Promise((resolve, reject) => {
+        db.query(query, payload, function (error, results, fields) {
+            if (error) {
+                reject(error)
+            } else {
+                resolve(results)
+            }
+        })
+    })
+}
+
+exports.updateTask = (payload) => {
+    const query = `
+    UPDATE tasks
+    SET task_name = ?, task_description = ?, task_due_date = ?
+    WHERE task_id = ?`
+
+    return new Promise((resolve, reject) => {
+        db.query(query, payload, function (error, results, fields) {
+            if (error) {
+                reject(error)
+            } else {
+                resolve(results.changedRows)
+            }
+        })
+    })
+}
+
+exports.deleteTask = (payload) => {
+    const query = `
+    UPDATE tasks
+    SET is_deleted = true
+    WHERE task_id = ?`
+
+    return new Promise((resolve, reject) => {
+        db.query(query, payload, function (error, results, fields) {
+            if (error) {
+                reject(error)
+            } else {
+                resolve(results.changedRows)
             }
         })
     })
