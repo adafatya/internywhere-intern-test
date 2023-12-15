fe4603f5758f3e0dad328bae2df274b0d8618cd9
adafatya <aufaditya1895@gmail.com>
2023-12-15 17:19:19 +0700

    change class to className

--- frontend/src/components/TaskTableItem.jsx
+++ frontend/src/components/TaskTableItem.jsx
@@ -11,18 +11,18 @@ function TaskTableItem ({ no, name, due_date, id }) {
             <td>{name}</td>
             <td>{formatDate(new Date(due_date))}</td>
             <td>
-            <button class="button is-info">
-                <span class="icon is-small">
+            <button className="button is-info">
+                <span className="icon is-small">
                 <FaEye />
                 </span>
             </button>
-            <button class="button is-warning">
-                <span class="icon is-small">
+            <button className="button is-warning">
+                <span className="icon is-small">
                 <FaPen />
                 </span>
             </button>
-            <button class="button is-danger">
-                <span class="icon is-small">
+            <button className="button is-danger">
+                <span className="icon is-small">
                 <FaTrash />
                 </span>
             </button>
